2c1aa6c55ae62916ae182f40c74a4ed9582f6e13
pedropmedina <pedrodelilia@gmail.com>
2018-11-12 15:05:04 -0500

    Add few more built-in methods

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 # keyword arguments allows to assign value to arguments
 # regardless of the order.
+
+
 def say_hi(first_name, last_name):
     return f"Hi {first_name} {last_name}"
 
@@ -9,6 +11,8 @@ greeting = say_hi(last_name="Medina", first_name="Luca")
 print(greeting)  # Hi Luca Medina
 
 # default parameters. This is similar to JS
+
+
 def say_hello(name="Luca"):
     return f"Hello there {name}"
 
@@ -44,6 +48,8 @@ print(increment())  # 3
 # nonlocal keyword is use to manipulate variables
 # within the outer functions scope. Important when
 # using closure
+
+
 def outer():
     """Increses counter in the parent function's scope using closure"""
     counter = 0
@@ -65,11 +71,14 @@ def outer():
 # This is how we access the documation for a function __doc__
 # To set the documentation to a function we use """ """ as
 # seen above in the outer function
-print(outer.__doc__)  # Increses counter in the parent function's scope using closure
+# Increses counter in the parent function's scope using closure
+print(outer.__doc__)
 
 # Gathering is achive with the * symbol. The is the equivalent
 # to rest ... in javascript. Pythons returns the args in a
 # tuple data structure
+
+
 def sum_all_nums(*args):
     sum = 0
     for num in args:
@@ -84,6 +93,8 @@ print(sum_all_nums(1, 2, 3, 4))  # 10
 # will  return a dictionaty {} instead of a tuple
 # This approach is useful when passing keywords to
 # arguments to functions
+
+
 def fav_colors(**kwargs):
     print(kwargs)
     for k, v in kwargs.items():
@@ -110,8 +121,84 @@ print(add_to_person)  # {"name": "Pedro", "age": 29, "gender": "male"}
 
 # lambda functions
 # lambda functions are the equivalent of arrow functions in JS
-func = lambda num: num * num
+
+
+def func(num):
+    return num * num
+
+
 print(func(3))  # 9
 
 func_1 = lambda num_1: lambda num_2: num_1 * num_2
+
 print(func_1(3)(4))  # 12
+
+# map
+numbers = [1, 2, 3, 4, 5, 6]
+doubles = list(map(lambda x: x * 2, numbers))  # map returns an map object
+print(doubles)  # [2, 4, 6, 8, 10, 12] we use list() on map object
+
+# filter
+evens = list(filter(lambda x: x % 2 == 0, numbers))
+print(evens)  # [2, 4, 6]
+
+names_with_b = list(
+    map(
+        lambda name: name.upper(),
+        filter(lambda name: name[0].lower() == "b", names),
+    )
+)
+print(names_with_b)
+
+# Built-in functions:
+
+# all
+# Returns True if all elements are thruthy, else returns False
+names_1 = ["Bianca", "Blanca", "Bana", "Banana"]
+names_2 = ["Bianca", "Blanca", "Bana", "Tana"]
+
+names_with_B = [name[0] == "B" for name in names_1]
+most_names_with_B = [name[0] == "B" for name in names_2]
+
+print(names_with_B)  # [True, True, True, True]
+print(most_names_with_B)  # [True, True, True, False]
+
+print(all(names_with_B))  # True
+print(all(most_names_with_B))  # False
+
+# any
+# Returns True if any element is thruthy
+print(any(num % 2 == 0 for num in [1, 2, 3, 4]))  # True
+
+# sorted
+# Sorts and returns new list. It works with lists and tuples
+numbers_1 = [1, 66, 534, 33, 2, 5]
+songs = [
+    {"title": "Black hole sun", "artist": "Soundgarden", "playcount": 44},
+    {"title": "Black", "artist": "Pearl Jam", "playcount": 134},
+    {"title": "One", "artist": "Metallica", "playcount": 3},
+]
+
+print(sorted(numbers_1))  # [1, 2, 5, 33, 66, 534]
+print(sorted(numbers_1, reverse=True))  # [534, 66, 33, 5, 2, 1]
+print(numbers_1)  # [1, 66, 534, 33, 2, 5]
+
+# sorted_by_playcount = sorted(songs, key=lambda song: song["playcount"])
+sorted_by_playcount = sorted(
+    songs, key=lambda song: song["playcount"], reverse=True
+)
+print(sorted_by_playcount)
+
+# min and max
+# we can pass in list, dicts, tuples, string, numbers
+print(min(28, 323, 1, 24, 36))
+print(max(28, 323, 1, 24, 36))
+print(max(numbers_1))
+print(min((1, 2, 3, 4, 6)))
+print(min("hello"))
+print(max(songs, key=lambda song: len(song["title"])))
+print(max(songs, key=lambda song: len(song["title"]))["title"])
+
+# reversed
+# Retuns a new reversed object without mutating the original
+print(list(reversed(numbers_1)))
